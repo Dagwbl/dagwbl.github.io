3a69db752de1bff1526e309314f3916261c5919a
Dagwbl <dagwbl@qq.com>
2025-04-01 11:14:31 +0800

    feat: add a commit checking before commiting

---
+++ commitlint.config.js
@@ -0,0 +1,8 @@
+// commitlint.config.js
+module.exports = {
+    // https://github.com/conventional-changelog/commitlint/tree/master/%40commitlint/config-conventional
+    extends: ["@commitlint/config-conventional"],
+    rules: {
+      // 自定义规则
+    }
+  };
